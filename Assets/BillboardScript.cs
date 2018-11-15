0067f11d749386ca80a8a59eb2dcffba29fc9e1b
dairong <ambera23325a@gmail.com>
2018-11-15 15:50:50 +0800

    first commit

---
+++ Assets/BillboardScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BillboardScript : MonoBehaviour {
+	public Camera my_camera;
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.LookAt (transform.position + my_camera.transform.rotation*Vector3.back,
+			my_camera.transform.rotation*Vector3.down);
+	}
+}
---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+	public float max_Health = 100.0f;
+	public float cur_Health = 0.0f;
+	public GameObject healthBar;
+	// Use this for initialization
+	void Start () {
+		cur_Health = max_Health;
+		InvokeRepeating ("decreasehealth",1.0f,1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void decreasehealth()
+	{
+		cur_Health -= 2;
+		float calc_Health = cur_Health / max_Health;
+		SetHealthBar (calc_Health);
+	}
+	public void SetHealthBar(float myHealth)
+	{
+		healthBar.transform.localScale = new Vector3 (myHealth,healthBar.transform.localScale.y,healthBar.transform.localScale.z);
+	}
+}
